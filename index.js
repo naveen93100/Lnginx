4c3a3c3e299bd0f91653a64879902ace2e4629cf
naveen93100 <naveen9310097505@gmail.com>
2026-03-10 16:30:06 +0530

    firstcommit

---
+++ index.js
@@ -0,0 +1,11 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  return res.status(200).json({ success: true, message: "Nginx is working" });
+});
+
+app.listen(1008, () => {
+  console.log("Server is running on port:", 1008);
+});
