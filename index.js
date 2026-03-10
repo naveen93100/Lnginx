1973c41dbe2baf4fabf0c9a56f35cfad70f1b6ea
naveen93100 <naveen9310097505@gmail.com>
2026-03-10 18:29:22 +0530

    d

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ import express from "express";
 const app = express();
 
 app.get("/api", (req, res) => {
-  return res.status(200).json({ success: true, message: "Nginx is working" });
+   res.send("Nginx is working");
 });
 
 app.listen(1009, () => {
